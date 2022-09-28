94b8a3194eb2fe3dd57bcc4f6e725078074132e7
MaxLMTR <94896515+MaxLMTR@users.noreply.github.com>
2022-09-28 09:23:34 +0200

    first commit

---
+++ tableau3.php
@@ -0,0 +1,8 @@
+//maxou
+<?php
+$marque = array("volvo","BMW","Toyota", "Peugeot","Renault");
+$i=0;
+foreach ($elem as $marque) {
+	echo "Vente de : " . $elem . "<br/>";
+}
+?>
---
+++ tableau4.php
@@ -0,0 +1,11 @@
+//maxou
+<?php
+
+$marque = array("volvo","BMW","Toyota");
+
+for ($i=0; $i<count($marque); $i=++;)
+{
+	echo "Achat de : " . $marque[$i] . "<br/>";
+}
+
+?>
